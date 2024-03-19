07f1eee26f3f6ea904030896ed526080857abfd6
dharam-in <dharamrajyadav6312@gmail.com>
2024-03-19 22:15:06 +0530

    Basic Setup and Database Connected

---
+++ Backend/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {config} from 'dotenv';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import userRouter from './routes/userRouter.js';
+import jobRouter from './routes/jobRouter.js';
+import applicationRouter from './routes/applicationRouter.js';
+import {dbConnection} from './database/dbConnection.js'
+
+
+config({path: './config/config.env'});
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"]
+}))
+
+app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}))
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+dbConnection()
+
+export default app;
